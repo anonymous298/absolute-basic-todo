47452d3d292725b39c5c4ae4c78c5b894e441121
anonymous298 <tackletalha@gmail.com>
2025-05-31 17:24:59 +0500

    js added for localstorage

--- script.js
+++ script.js
@@ -2,11 +2,60 @@ let addButton = document.getElementById('addToDo');
 let todoContainer = document.querySelector('.todo-container');
 let todoInputBox = document.querySelector('#todoInput');
 
+if (localStorage.length > 1) {
+    for (const key in localStorage) {
+        if (typeof localStorage[key] === 'string' && localStorage[key] !== '') {
+
+            let div = document.createElement('div');
+            div.className = 'todos';
+            div.innerHTML = `<div class="todos">
+            <p>${localStorage[key]}</p>
+            <button id="removeToDo">Remove</button>
+            </div>`
+            
+            div.querySelector('#removeToDo').addEventListener('click', () => {
+                div.remove();
+                localStorage.removeItem(div.querySelector('p').innerHTML, div.querySelector('p').innerHTML);
+                // console.log(div.querySelector('p').innerHTML)
+            });
+            
+            todoContainer.append(div);
+        }
+    }
+}
+
+todoInputBox.addEventListener('change', () => { 
+    if (todoInputBox.value === '') {
+        alert('Please Enter some To Do before adding');
+    }
+    
+    else {
+        let div = document.createElement('div');
+        div.className = 'todos';
+        div.innerHTML = `<div class="todos">
+        <p>${todoInputBox.value}</p>
+        <button id="removeToDo">Remove</button>
+        </div>`
+
+        localStorage.setItem(todoInputBox.value, todoInputBox.value);
+        
+        div.querySelector('#removeToDo').addEventListener('click', () => {
+            div.remove();
+            localStorage.removeItem(div.querySelector('p').innerHTML, div.querySelector('p').innerHTML);
+            // console.log(div.querySelector('p').innerHTML)
+        });
+        
+        todoContainer.append(div);
+        todoInputBox.value = '';
+
+    } 
+})
+
 addButton.addEventListener('click', () => {
     if (todoInputBox.value === '') {
         alert('Please Enter some To Do before adding');
     }
-
+    
     else {
         let div = document.createElement('div');
         div.className = 'todos';
@@ -17,12 +66,15 @@ addButton.addEventListener('click', () => {
         
         div.querySelector('#removeToDo').addEventListener('click', () => {
             div.remove();
+            localStorage.removeItem(div.querySelector('p').innerHTML, div.querySelector('p').innerHTML);
+            // console.log(div.querySelector('p').innerHTML)
         });
         
         todoContainer.append(div);
         todoInputBox.value = '';
+
+        localStorage.setItem(todoInputBox.value, todoInputBox.value);
     } 
 });
-
 
 
